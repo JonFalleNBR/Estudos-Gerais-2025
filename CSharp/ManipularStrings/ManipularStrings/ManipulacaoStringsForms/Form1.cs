846cfa27abef85df4824c6016a1890cc53caf309
Jonathan-Silva <91348218+JonFalleNBR@users.noreply.github.com>
2025-04-28 22:15:46 -0300

    feat- adicionando conteudo aula manipulacao de strings

---
+++ CSharp/ManipularStrings/ManipularStrings/ManipulacaoStringsForms/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManipulacaoStringsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+
+            #region Metodo Contains
+            //string texto = "Tipo de Variavel Alfa Numerica"; // alterar o tipo em minusculo para ser enconttado na validação
+
+            //// Exemplo do Metodo Contains()
+
+            //// verifica se a string "texto" contém a palavra "tipo" - contains 
+            //if (texto.ToLower().Contains("tipo")) // como o csahrp é case sensitive, o trecho "tipo" não é encontrado na string "texto" - a menos que agregue a validação com o toLower
+            //{
+
+            //    label1.Text = "A string contém a palavra 'tipo'";
+
+            //}
+            //else
+            //{
+            //    label1.Text = "A string Não contem o trecho tipo";
+
+            //}
+            #endregion
+
+
+            #region Exmeplo do Metodo ToString
+            // exemplo pratico do toString 
+            // toda variavel tem esse metodo, uma vez que ele serve para converter a variavel em string 
+            // o toString é um metodo herdado da classe object, que é a superclasse de todas as classes do C#
+
+
+            //int num1 = 10;
+            //label1.Text = num1.ToString(); // converte o inteiro em string
+            #endregion
+
+
+            #region Exemplo do Metodo ToLower / ToUpper
+            //string texto = "Exemplo";
+
+            //label1.Text = texto.ToUpper(); // Retorna uma copia da String em letras maiusculas
+
+            //string texto2 = "EXEMPLO";
+
+            //label1.Text = texto2.ToLower(); // Retorna uma copia da String em letras minusculas
+            #endregion
+
+
+
+            #region Exemplo LastIndexOf
+
+            string nome = "João da Silva";
+
+
+          ; // Retorna a posição do primeiro caracter da string "da" dentro da string "nome"
+            // ele irá retornar apenas se estiver minusculo, uma vez que o C# é case sensitive
+            // Ele irá sempre retornar semrpe a primeira posição do caracter encontrado
+
+
+            int indice = nome.IndexOf("da"); // Retorna a posição do primeiro caracter da string "da" dentro da string "nome" 
+
+            //label1.Text = "indice: " + indice2.ToString(); // Retorna a posição do primeiro caracter da string "da" dentro da string "nome"
+
+
+            int indice2 = nome.IndexOf("ilv", 4); // Após a posição index 4, ele irá procurar a string "ilv" dentro da string "nome"
+                                                  // Isso é muito util para fazer buscas dentro de strings, uma vez que ele irá
+                                                  // retornar a posição do primeiro caracter da string "ilv" dentro da string "nome"
+
+            label1.Text = "indice: " + indice2.ToString(); // Retorna a posição do primeiro caracter da string "da" dentro da string "nome"
+
+            #endregion
+
+        }
+
+
+
+
+    }
+}
---
+++ CSharp/ManipularStrings/ManipularStrings/ManipulacaoStringsForms/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ManipulacaoStringsForms")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ManipulacaoStringsForms")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4edee65a-209e-428d-8ca8-730b34ebc8b1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
