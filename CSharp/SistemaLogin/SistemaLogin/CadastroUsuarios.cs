2cad79aa32ac2a08128462fcf2ac0e1221bbd366
Jonathan-Silva <91348218+JonFalleNBR@users.noreply.github.com>
2025-05-13 21:39:00 -0300

    feat-classe_Usuarios_CadastroUsuarios

---
+++ CSharp/SistemaLogin/SistemaLogin/CadastroUsuarios.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaLogin
+{
+    class CadastroUsuarios
+    {
+
+        // Classe responsavel por cadastrar os usuarios e verificar se o usuario ja existe
+
+        private List<Usuario> usuarios = new List<Usuario>();
+
+
+        public void CadastrarUsuario(string nome, string senha)
+        {
+            // Verifica se o usuario ja existe
+            if (usuarios.Any(u => u.Nome == nome))
+            {
+                throw new Exception("Usuario ja existe");
+                MessageBox.Show("Usuario ja existe na base de Dados", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            // Adiciona o usuario a lista
+            usuarios.Add(new Usuario(nome, senha));
+        }
+
+
+        public bool Login(string nome, string senha)
+        {
+
+           
+            foreach (Usuario usuario in usuarios)
+            {
+                // Verifica se o usuario e senha estao corretos e existem na lista
+                if (usuario.Nome == nome && usuario.Senha == senha)
+                {
+             
+                    return true;
+                }
+                              
+            }
+
+            return false;
+
+
+        }
+    }
+}
---
+++ CSharp/SistemaLogin/SistemaLogin/Usuario.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaLogin
+{
+    class Usuario
+    {
+       
+
+        public string Nome { get; set; }
+
+        public string Senha { get; set; }
+
+
+
+        public Usuario(string nome, string senha)
+        {
+            Nome = nome;
+            Senha = senha;
+        }
+
+
+        public String NomeUsu
+        {
+            get { return Nome; }
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Usuario Invalido");
+                    MessageBox.Show("Informe um usuario válido", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                Nome = value;
+            }
+        }
+
+
+        public String SenhaUsu
+        {
+
+
+            get { return Senha; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Senha Invalida");
+                    MessageBox.Show("Informe uma senha Válida", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                Senha = value;
+            }
+
+        }
+    }
+}
+
