12355db4a0498791a75ab98a67ea614479098c35
Jonathan-Silva <91348218+JonFalleNBR@users.noreply.github.com>
2025-05-22 16:36:07 -0300

    feature-exemplo_Lista_Encadeada

---
+++ Java/estrutura-de-dados/Test/ListaEncadeadaTeste.java
@@ -0,0 +1,7 @@
+public static void main(String[]args){
+
+    ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+        lista.adiciona(1);
+
+
+        }
---
+++ Java/estrutura-de-dados/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Scanner sc = new Scanner(System.in);
+//        String nome;
+//
+//        System.out.println("Informe seu nome: ");
+//        nome = sc.nextLine();
+//
+//        System.out.println("Ola " + nome)
+
+
+        ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+        lista.Adiciona(1);
+
+        System.out.println(lista);
+
+
+
+
+
+
+    }
+}
