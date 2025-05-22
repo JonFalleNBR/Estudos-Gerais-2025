de2b647845cd94be39946da6b5915dbb9d9c96ca
Jonathan-Silva <91348218+JonFalleNBR@users.noreply.github.com>
2025-05-21 21:39:59 -0300

    feat-exercicio_fibonnacci

--- Kotlin/kotlin_code_exercises/src/main/kotlin/main.kt
+++ Kotlin/kotlin_code_exercises/src/main/kotlin/main.kt
@@ -1,7 +1,7 @@
 fun main() {
     //println("Hello, Kotlin!")
 
-
+/*
     var nome : String;
 
     print("Informe seu Nome:")
@@ -9,4 +9,46 @@ fun main() {
 
     println ("Ola $nome");
 
-}
+
+
+    // loop simples
+    val asterisco = "*";
+
+    for(i in 0..10){
+        println(asterisco.repeat(i))
+    }
+*/
+// Sequencia de Fibonnacci
+
+    var a = 0;
+    var b = 1;
+    var c : Int;
+
+
+    print("Quantos serão os valores ? ")
+    val input = readLine();
+    val valores = input?.toIntOrNull() ?: 0;
+
+    println("Sequencia de Fibonnaci com $valores valores");
+
+    for (i in 0 until valores) {
+
+
+        if(i < valores -1){
+           print("$a, ")
+
+
+        }else{
+
+            print("$a");
+
+        }
+
+        c = a + b;
+        a = b;
+        b = c;
+
+    }
+    }
+
+
