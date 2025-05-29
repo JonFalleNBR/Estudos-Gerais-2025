ae348812b3caffea362aa7d9e613d5f584f56581
Jonathan-Silva <91348218+JonFalleNBR@users.noreply.github.com>
2025-05-28 21:06:06 -0300

    feature-estudo_Arrays_in_Kotlin

---
+++ Kotlin/Estudos_Linguagem_Kotlin/src/main/kotlin/collections/Arrays_Lists.kt
@@ -0,0 +1,74 @@
+package collections
+
+fun main (args : Array<String>){
+
+
+    val array = arrayOf("João", "Marcos", "Lucas", "Mateus")
+
+    val lista = listOf("Java", "Kotlin", "Python", "JavaScript")
+
+    val arrayList = arrayListOf("Laranja", "Maçã", "Banana", "Pera")
+
+
+    for (i in array.indices){
+        print("Elemento ${array[i]} na posição $i \n")
+
+    }
+
+
+// ArrayMisto
+   val mixed = arrayListOf("Java", 10, 3.14, true, 'F') // ArrayList pode conter diferentes tipos de dados
+
+    for (item in mixed ){
+        print("$item \n")
+    }
+
+    // Array de numeros Inteiros
+    val numeros = intArrayOf(1,2,3,4)
+
+    // array de numeros do tipo double
+    val numerosDouble = doubleArrayOf(3.4,2.5,1.2)
+
+    // arrays do tipo Boolean
+    val booleanos = booleanArrayOf(true, false)
+
+    // Nenhum desses arrays pode ser alterado após a criação, ou seja, são imutáveis.
+
+
+    mixed[0] //como acesasr uma posição especifica do array ou imprimi-la - use o colchetes como em qualquer linguagem basica de programaçao
+
+    print(mixed[1]) // numero 10
+
+    val str = "Ola Mundo" // string aqui é uma coleção de caracteres, não um array
+    println(str[0])
+
+    // exemplo de como concatenar arrays
+
+    val array1 = arrayOf("Primeiro", "Segundo", "terceiro")
+    val array2 = arrayOf("Quaarto", "Quinto", "Sexto")
+    val allSrings = array1 + array2 // concatena os dois arrays
+    println("\n array concatenado:  ${allSrings.size}")
+
+
+    val empty : Boolean = numeros.isEmpty();
+
+    if(array1.contains("Primeiro")){
+        print("\n O array contém o elemento 'Primeiro'")
+
+    }else{
+        println("\n O array não contém o elemento Primeiro")
+
+    }
+
+    /*
+
+    use arrayOf para criar um novo Array
+    use listOf para criar uma nova Lista
+
+   use arrayListOf para criar uma nova ArrayLista
+
+   A diferença da ArrayLista para os dois primeiros é que essa permite edição como adicionar ou remover um item -
+   enquanto os outros dois são imutaveis , ou seja , não podem ser alterados após a criação.
+
+     */
+}
