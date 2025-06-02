fe9841ae1a09a4216a99b5014ea3fe24762c7410
Jonathan-Silva <91348218+JonFalleNBR@users.noreply.github.com>
2025-06-02 20:46:37 -0300

    feature-mais_Sobre_loop_for

--- Kotlin/Estudos_Linguagem_Kotlin/src/main/kotlin/collections/Loops.kt
+++ Kotlin/Estudos_Linguagem_Kotlin/src/main/kotlin/collections/Loops.kt
@@ -1,5 +1,7 @@
 package collections
 
+import kotlin.system.exitProcess
+
 fun main(args: Array<String>){
 
 
@@ -34,6 +36,31 @@ fun main(args: Array<String>){
             }
 
         }
+
+// in no for cria um range - o for no kotlin é bem mais simples que em outras linguagens fortemente tipadas
+
+    var list = listOf("Kotlin", "Java", "Python", "JavaScript")
+
+    for(item in list){ // semelhante ao foreach em outras linguagens - percorre uma lista de objetos
+        print(item) // imprime os itens da lista
+
+
+    }
+
+    for((index, value) in list.withIndex()){
+        println("Item na posição $index é $value") // imprime o indice e o valor do item na lista
+
+        /*
+        Semelhante ao forEach em outras linguagens, o withIndex() permite acessar o índice e o valor do item na lista.
+        e tem a forma de fazer a mesma coisa aqui da seguinte forma
+
+        for(item in list){
+            print("Item esta na posição $item.indices \n" +
+                  "Item na posição $item é ${list[item]} \n"
+        }
+         */
+
+    }
 
 
 }
