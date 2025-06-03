309371c0923f2a4dab0e5e1deab4386ed3200ac7
Jonathan-Silva <91348218+JonFalleNBR@users.noreply.github.com>
2025-06-02 21:19:01 -0300

    feature-nomear_loops_com_@

--- Kotlin/Estudos_Linguagem_Kotlin/src/main/kotlin/collections/Loops.kt
+++ Kotlin/Estudos_Linguagem_Kotlin/src/main/kotlin/collections/Loops.kt
@@ -21,21 +21,21 @@ fun main(args: Array<String>){
 
 
 
-        print("Informe o numero que voce quer ver a tabuada: ")
-        val input = readLine()
-        numero = input?.toIntOrNull() ?: 0 ;
+    print("Informe o numero que voce quer ver a tabuada: ")
+    val input = readLine()
+    numero = input?.toIntOrNull() ?: 0 ;
 
-        if (numero <= 0){
-            println("Numero Invalido - Não é possivel realizar Multiplicação com numeros menores ou iguais a 0 ou maiores que 10. \n")
+    if (numero <= 0){
+        println("Numero Invalido - Não é possivel realizar Multiplicação com numeros menores ou iguais a 0 ou maiores que 10. \n")
 
-        }else{
-            for (i in 1..10){
-                val resultado = numero * i;
-                println("$numero x $i = $resultado")
-
-            }
+    }else{
+        for (i in 1..10){
+            val resultado = numero * i;
+            println("$numero x $i = $resultado")
 
         }
+
+    }
 
 // in no for cria um range - o for no kotlin é bem mais simples que em outras linguagens fortemente tipadas
 
@@ -65,18 +65,18 @@ fun main(args: Array<String>){
 
     // Sobre loops While e do-while
 
-        var x = 9;
+    var x = 9;
 
-       while(x >= 0){
-           print("\n $x")
-           x -= 1; // ou x--
-           // começou em 9 , e foi decrementando ate chegar a 0
-           if(x == 0){
+    while(x >= 0){
+        print("\n $x")
+        x -= 1; // ou x--
+        // começou em 9 , e foi decrementando ate chegar a 0
+        if(x == 0){
 
-               print ("\nValor chegou a 0 amigue :D")
-               exitProcess(0);
-           }
-       }
+            print ("\nValor chegou a 0 amigue :D")
+            //exitProcess(0);
+        }
+    }
 
     // em um loop while ele irá criar um loop enquanto a condição for verdadeira
 
@@ -92,6 +92,7 @@ fun main(args: Array<String>){
 
     }
 
+
     var user = object {
         var isOnline = true
     }
@@ -103,9 +104,55 @@ fun main(args: Array<String>){
         if(input != null && input.isNotEmpty()){
             print("Ate mais! \n")
             user.isOnline = false // altera o estado do usuario para offline
-            exitProcess(0)
+            //exitProcess(0)
+
+        }
+        return // sai do loop e encerra o programa
+
+    }
+
+
+    // uso do Break e Continue
+
+    for(c in "python"){
+        if ( c == 'o'){
+            break // interrompe o loop quando encontrar a letra 'o'
+
+        }
+        print(c)
+
+    }
+
+
+    println("------------------------------------------")
+
+    var lista = listOf("Book", "Table", "Chair", "Pen")
+
+    for(str in list){
+        if(!str.contains('o')){
+            continue
 
         }
 
+        print(str);
+
+    }
+// Em Kotlin não e muito comum ou recomendado o uso do Break ou COntinue, mas caso necessario é um recurso bom para interações entre fluxos de controle dentro dos loops
+
+
+
+
+    // como nomear os loops
+
+    outer@ for(i in 1 .. 10){
+        for(j in 1..10){
+            if(i - j == 5 ){
+                break@outer
+            }
+            print("$i - $j \n")
+        }
+        // ele vai parar o loop externo quando a condição for atendida
+        // foi usado o nome no loop externo para que o Break aponte para ele e funcione corretamente
+        // para definir um nome use o @ antes no nome do Loop
     }
 }
