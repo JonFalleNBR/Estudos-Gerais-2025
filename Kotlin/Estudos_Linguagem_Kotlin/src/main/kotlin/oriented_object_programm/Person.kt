16afc744ca259064c0caf5ef48c91e829d090cef
Jonathan-Silva <91348218+JonFalleNBR@users.noreply.github.com>
2025-06-09 22:06:26 -0300

    feature-about_oop_in_kotlin

---
+++ Kotlin/Estudos_Linguagem_Kotlin/src/main/kotlin/oriented_object_programm/Person.kt
@@ -0,0 +1,17 @@
+package oriented_object_programm
+
+class Person {
+
+    var name: String = "Jon";
+    var age : Int = 18;
+
+}
+
+fun main(args: Array<String>) {
+    val person = Person()
+    println("Name: ${person.name}, Age: ${person.age}")
+
+
+    person.name = "Michelle"
+    person.age = 17
+}
