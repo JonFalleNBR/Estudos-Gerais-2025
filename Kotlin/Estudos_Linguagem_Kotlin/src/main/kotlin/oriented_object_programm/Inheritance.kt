89c7505d356cd327bbf4a13e25cdd838461325a0
Jonathan-Silva <91348218+JonFalleNBR@users.noreply.github.com>
2025-06-17 22:30:48 -0300

    feature-training_Inheritance

--- Kotlin/Estudos_Linguagem_Kotlin/src/main/kotlin/oriented_object_programm/Inheritance.kt
+++ Kotlin/Estudos_Linguagem_Kotlin/src/main/kotlin/oriented_object_programm/Inheritance.kt
@@ -1,6 +1,6 @@
 package oriented_object_programm
 
-class Pessoa(val nome: String, val idade: Int) {
+open class Pessoa(open val nome: String, open val idade: Int) {
 
     init{
         println("Pessoa criada: Nome = $nome, Idade = &idade" +
@@ -12,7 +12,54 @@ class Pessoa(val nome: String, val idade: Int) {
 
     }
 
+    fun cumprimentar(): String{
+        val message = "Ola $nome, tudo bem ?"
+        println(message)
 
+        return message
+
+    }
+
+
+    fun anoNascimento(): Int{
+        val anoNascimento: Int = 2025 - idade;
+        print("Eu nasci em $idade" +
+                "\n")
+        return anoNascimento
+
+    }
+
+
+}
+
+class Aluno(override val nome: String, override var idade:Int, val nota: Double): Pessoa(nome, idade){
+
+    fun CalculaNota(nota1: Double, nota2: Double, nota3: Double): Double{
+        val media = (nota1 + nota2 + nota3) / 3
+
+        if(media >  6.0){
+            println("Aluno $nome aprovado com media: %.2f".format(media) + "\n")
+
+        }else{
+            println("Aluno $nome reprovado com media: %.2f".format(media) + "\n")
+
+        }
+
+        return media
+
+    }
+
+}
+
+
+fun main(args: Array<String>){
+
+
+    val aluno = Aluno("João", 20, 8.5)
+    aluno.falar()
+    aluno.cumprimentar()
+    aluno.anoNascimento()
+    aluno.CalculaNota(8.0, 9.0, 7.5)
 
 
 }
