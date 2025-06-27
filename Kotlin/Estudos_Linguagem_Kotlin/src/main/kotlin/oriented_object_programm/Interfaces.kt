d1b5eb98770ffa13014e6706d19f8b18d10ff96b
Jonathan-Silva <91348218+JonFalleNBR@users.noreply.github.com>
2025-06-26 21:52:58 -0300

    feature-about_interfaces

--- Kotlin/Estudos_Linguagem_Kotlin/src/main/kotlin/oriented_object_programm/Interfaces.kt
+++ Kotlin/Estudos_Linguagem_Kotlin/src/main/kotlin/oriented_object_programm/Interfaces.kt
@@ -9,20 +9,45 @@ interface Driveable {
     fun drive();  // todo metodo de uma interface é por padrão abstrato
 }
 
-class Car(val color: String): Driveable{
+
+interface Buildable{
+    val timeRequired: Int; // variável para armazenar o tempo necessário para construir a moto
+
+    // a variável de uma interface é por padrão abstrata, ou seja, não tem implementação, mas deve ser implementada pelas classes que a implementam tal qual os metodos
+
+    fun build(); // outro metodo de uma interface, que deve ser implementado pelas classes que a implementam
+
+}
+
+
+class Car(val color: String): Driveable, Buildable{
+
+        override val timeRequired = 120 // tempo necessário para construir o carro, implementando a interface Buildable
 
     override fun drive (){
         println("DRIVING CAR")
 
     }
 
+    override fun build(){
+        println("BUILDING CAR, and it takes $timeRequired minutes to build it.")
+
+    }
+
 }
 
-class Motorcycle(val color: String): Driveable{
+class Motorcycle(val color: String): Driveable, Buildable{
 
+    override val timeRequired = 60
 
     override fun drive(){
         println("DRIVING MOTORCYCLE")
+    }
+
+    override fun build(){
+
+
+        println("BUILDING MOTORCYCLE, and it takes $timeRequired minutes to build it.")
     }
 
 }
@@ -32,9 +57,13 @@ fun main(args: Array<String>){
 
     car.drive()  // chama o metodo drive da classe Car que implementa a interface Driveable para o carro
 
+    car.build() // chama o metodo build da classe Car que implementa a interface Buildable para o carro
+
     val motorcycle = Motorcycle("Blue")
 
     motorcycle.drive()  // chama o metodo drive da classe Motorcycle que implementa a interface Driveable para a moto
+
+    motorcycle.build() // chama o metodo build da classe Motorcycle que implementa a interface Buildable para a moto
 
     print("Motorcycle color: ${motorcycle.color}" + "\n")
 
