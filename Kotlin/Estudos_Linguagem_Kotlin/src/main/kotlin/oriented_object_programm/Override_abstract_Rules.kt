710275b8a150d03bbff07ac9e2184e1f5c325c37
Jonathan-Silva <91348218+JonFalleNBR@users.noreply.github.com>
2025-07-03 21:50:44 -0300

    feature-about_overrides_and_abstract_rules

---
+++ Kotlin/Estudos_Linguagem_Kotlin/src/main/kotlin/oriented_object_programm/Override_abstract_Rules.kt
@@ -0,0 +1,50 @@
+package oriented_object_programm
+
+abstract class Course(val topic: String, val price: Double){
+
+    open fun learn(){
+        println("Learning in the $topic Course")
+
+    }
+
+
+}
+
+interface Learnable{
+    fun learn(){
+        println("Learning...")
+    }
+
+}
+
+open class KotlinCourse(): Course("Kotlin", 99.99), Learnable{
+
+    override final fun learn(){
+        //super.learn() // chama o metodo paadrão da classe Course - para especificar qual metodo learn chamar, visto que tanto a classe base Course quanto a interface Learnable possuem um metodo learn - usa-se o
+         // <> nesse caso
+        super<Course>.learn() // chama o metodo learn da classe base Course para imprimir a mensagem padrão e evitar codigo repetido
+        super<Learnable>.learn() // chama o metodo learn da interface Learnable para imprimir a mensagem padrão e evitar codigo repetido
+        println("First you will learn the basics of Kotlin")
+
+    }
+
+}
+
+class speacialKotlinCourse(): KotlinCourse(){
+    /*
+    override fun learn(){
+        println("Then you will learn advanced topics of Kotlin")
+
+    }
+    o final na classe KotlinCourse impede que essa função seja sobrescrita em classes filhas
+    */
+
+
+}
+
+fun main (args: Array<String>){
+    val course = KotlinCourse()
+    course.learn()
+
+
+}
