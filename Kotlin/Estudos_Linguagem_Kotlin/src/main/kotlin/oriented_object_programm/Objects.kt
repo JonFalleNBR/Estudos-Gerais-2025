e72d65bbd073b93f2a18f7930dbd6cc7d2c77f1d
Jonathan <jonathan.leoesdejuda@gmail.com>
2025-08-05 21:39:00 -0300

    feat-object_singletons

---
+++ Kotlin/Estudos_Linguagem_Kotlin/src/main/kotlin/oriented_object_programm/Objects.kt
@@ -0,0 +1,15 @@
+package oriented_object_programm
+
+object Cache {
+    val name = "Hyoercache"
+
+
+    fun retrieveDate(): Int {
+
+        return 0
+
+    }
+
+//exemplo de um singleton em kotlin - objeto que só pode ter uma instancia
+
+}
