1d6cfe4858d99e7d55606fbb8fb28810e6379de5
Jonathan <jonathan.leoesdejuda@gmail.com>
2025-08-05 21:47:24 -0300

    feat-more_about_object_singletons

--- Kotlin/Estudos_Linguagem_Kotlin/src/main/kotlin/oriented_object_programm/Objects.kt
+++ Kotlin/Estudos_Linguagem_Kotlin/src/main/kotlin/oriented_object_programm/Objects.kt
@@ -8,8 +8,22 @@ object Cache {
 
         return 0
 
+
     }
 
 //exemplo de um singleton em kotlin - objeto que só pode ter uma instancia
+// Singletons são uma classe que só pode ter uma única instância durante o tempo de execução do programa.
+    // nai poide ser instanciada como new Cache()
+
+    val nome = Cache.name
+    fun showName() = println("Cache name is $nome")
+
+}
+
+fun main(args: Array<String>){
+
+val CacheaName = Cache;
+    CacheaName.showName()
+
 
 }
