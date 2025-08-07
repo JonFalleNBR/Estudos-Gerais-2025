4d0619c4bcd3a9da3f03c6c42e9c108db92b322a
Jonathan <jonathan.leoesdejuda@gmail.com>
2025-08-06 22:07:08 -0300

    feat-hexadecimal_examples

--- Kotlin/Estudos_Linguagem_Kotlin/src/main/kotlin/oriented_object_programm/Color.kt
+++ Kotlin/Estudos_Linguagem_Kotlin/src/main/kotlin/oriented_object_programm/Color.kt
@@ -7,3 +7,18 @@ enum class Color {
     // Enum é uma classe especial que define um conjunto fixo de constantes, neste caso, as cores - AULA 46
 
 }
+
+fun main(args: Array<String>) {
+
+    // numeros entre 0..9 ->
+    // aula sobre sistema headecimal e binario
+    println(0x10) // 16 em decimal - é o 0 com todos os numeros entre 0 e 9 e letras A a F
+    println(0x1F) // 31 em decimal
+    println(0xFF) // 255 em decimal
+
+    println(0x000000) //preto
+    println(0xFFFFFF) // branco
+    println(0xFF0000) // vermelho
+    println(0x00FF00) // verde
+    println(0x0000FF) // azul
+}
