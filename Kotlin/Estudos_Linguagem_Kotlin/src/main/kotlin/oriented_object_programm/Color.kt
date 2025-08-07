7cf79a93fdb2f382107f9d5cb822de818b35c468
Jonathan <jonathan.leoesdejuda@gmail.com>
2025-08-06 22:43:39 -0300

    feat-binary_logic

--- Kotlin/Estudos_Linguagem_Kotlin/src/main/kotlin/oriented_object_programm/Color.kt
+++ Kotlin/Estudos_Linguagem_Kotlin/src/main/kotlin/oriented_object_programm/Color.kt
@@ -6,12 +6,22 @@ enum class Color(val rgb: Int) {
 
     // Enum é uma classe especial que define um conjunto fixo de constantes, neste caso, as cores - AULA 46
 
+
+
+
+    fun containsRed(): Boolean {
+
+        return this.rgb and 0xFF0000 != 0 // verifica se a cor contém o componente vermelho
+    }
+
 }
 
 fun main(args: Array<String>) {
 
     // numeros entre 0..9 ->
     // aula sobre sistema headecimal e binario
+
+    // hexadecimal
     println(0x10) // 16 em decimal - é o 0 com todos os numeros entre 0 e 9 e letras A a F
     println(0x1F) // 31 em decimal
     println(0xFF) // 255 em decimal
@@ -21,4 +31,21 @@ fun main(args: Array<String>) {
     println(0xFF0000) // vermelho
     println(0x00FF00) // verde
     println(0x0000FF) // azul
+
+
+
+
+    // binarios
+    println (0b1010) // 10 em decimal
+    println(0b00001010) // 10 em decimal
+    println(0b11111111) // 255 em decimal
+    println(0b11111111_00000000_00000000 == 0xFF0000) // isso é a cor
+
+
+
+    println(Color.RED.containsRed())
+    println(Color.GREEN.containsRed())
+    println(Color.BLUE.containsRed())
+    println(Color.YELLOW.containsRed())
+
 }
