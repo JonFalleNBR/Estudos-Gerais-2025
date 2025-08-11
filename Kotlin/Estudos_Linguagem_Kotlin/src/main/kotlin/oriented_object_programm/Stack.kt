8b6428652fd3aa400c1ef85b8eb74f432ef1b782
Jonathan <jonathan.leoesdejuda@gmail.com>
2025-08-11 20:53:20 -0300

    feat-generic_classes-DRY

---
+++ Kotlin/Estudos_Linguagem_Kotlin/src/main/kotlin/oriented_object_programm/Stack.kt
@@ -0,0 +1,22 @@
+package oriented_object_programm
+
+
+
+// DRY -> não repetir código, encapsulando a lógica de pilha em uma classe
+    class Stack<E>{
+
+
+
+        fun push(){
+            val array = intArrayOf(1,2,3,);
+
+        }
+
+        fun pop(){
+
+
+        }
+
+
+    }
+
