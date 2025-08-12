5d25599ab932ee3d5093b8dd60a22580ab90591e
Jonathan <jonathan.leoesdejuda@gmail.com>
2025-08-11 21:00:16 -0300

    feat-generic_classes-DRY

--- Kotlin/Estudos_Linguagem_Kotlin/src/main/kotlin/oriented_object_programm/Stack.kt
+++ Kotlin/Estudos_Linguagem_Kotlin/src/main/kotlin/oriented_object_programm/Stack.kt
@@ -6,9 +6,30 @@ package oriented_object_programm
     class Stack<E>{
 
 
+/*
+    Com essa logica eu defino uma classe como um tipi generico e posso usar qualquer tipo de dado como
+    parametro em uma classe filha que a for herdar
 
-        fun push(){
-            val array = intArrayOf(1,2,3,);
+    Exemplo de uso:
+
+    val intStack = Stack<Int>()
+    intStack.push(1)
+    intStack.push(2)
+    intStack.push(3)
+    intStack.pop() // remove o último item adicionado
+    println(intStack) // imprime o estado atual da pilha
+    val stringStack = Stack<String>()
+    stringStack.push("Hello")
+    stringStack.push("World")
+    stringStack.pop() // remove o último item adicionado
+    println(stringStack) // imprime o estado atual da pilha
+
+    acima temos exemplo da mesma classe e mesmo metodo manipulando diferentes tipos de dados, como Int e String.
+    Isso garante a não repetição de codigo
+
+ */
+        fun push(item: E){
+            val array = arrayOf(1,2,3,);
 
         }
 
