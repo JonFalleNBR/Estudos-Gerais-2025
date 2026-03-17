2ae90e8c3611b1aafb2c8e5d258e13e14b8f1e8d
Jonathan <jonathan.leoesdejuda@gmail.com>
2026-03-16 21:31:01 -0300

    feat: Relevant Big(O) anotations

---
+++ DataStructures/src/bigO/ExerciseTwo.js
@@ -0,0 +1,26 @@
+function anotherFunChallenge(input) {
+  let a = 5;
+  let b = 10;
+  let c = 50;
+
+  for (let i = 0; i < input; i++) {
+    let x = i + 1;
+    let y = i + 2;
+    let z = i + 3;
+  }
+  for (let j = 0; j < input; j++) {
+    let p = j * 2;
+    let q = j * 2;
+  }
+
+  let WhoAmI = "I Don`t Know";
+}
+
+/* 
+ O(4 + 5n)
+ but, we should ignore the constants and say that the time of complexity is O(n), or whatever 
+ is the predominant complexity of the code - that`s a value trick to interviews and 
+ identify quickly the complexity of the code, but in general, we should say that the
+  time complexity of this code is O(n) because the time of execution of the code is defined 
+ by the loops and the size of the input, and not by the constants.
+*/
