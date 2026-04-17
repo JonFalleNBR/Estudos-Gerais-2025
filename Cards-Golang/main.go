31d8c21855092364d2d784188c301b8caa9591ab
Jonathan <jonathan.leoesdejuda@gmail.com>
2026-04-16 22:47:58 -0300

    about append and Slices

--- Cards-Golang/main.go
+++ Cards-Golang/main.go
@@ -4,10 +4,19 @@ import "fmt"
 
 func main() {
 
-	card := newCard()
-	fmt.Println(card)
+	cards := []string{newCard(), newCard(), "Ace of Spades"}
+	cards = append(cards, "Six of Spades") // append adds a new element to the end of the slice and returns the updated slice
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 func newCard() string {
 	return "Five of Diamonds"
+}
+
+func deckSize() int {
+
+	return 53
 }
